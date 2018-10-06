e033d5ca86ad047a806455d63d8fd20b450dc97f
Tamas Kuzdi <taku2@betssongroup.com>
2018-10-06 20:44:46 +0200

    start webapi

---
+++ SportsbookAPI/SportsbookAPI.Web/Controllers/EventsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SportsbookAPI.Web.Models;
+
+namespace SportsbookAPI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        /// <summary>
+        /// Returns events in system
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<SportsbookEvent>), 200)]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var events = new List<SportsbookEvent>
+            {
+                new SportsbookEvent
+                {
+                    Id = 1,
+                    Name = "Manchester UTD - Newcastle",
+                    StartTime = new DateTime(2018, 10, 6, 18, 30, 0),
+                    StadiumId = 1
+                }
+            };
+
+            return Ok(events);
+        }
+    }
+}
---
+++ SportsbookAPI/SportsbookAPI.Web/Models/SportsbookEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsbookAPI.Web.Models
+{
+    public class SportsbookEvent
+    {
+        /// <summary>
+        /// Uniq identifier of event
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Date and time of the event's start
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// Publicly displayable title of the event
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Identifier of the stadium which hosts the event
+        /// </summary>
+        public int StadiumId { get; set; }
+    }
+}
